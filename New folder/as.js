6a991b0828bc001734eb4c4b71f2529fccb03cf6
Nikhil <ny542805@gmail.com>
2024-02-05 18:03:01 +0530

    uploade Css coding ninja project

---
+++ New folder/as.js
@@ -0,0 +1,34 @@
+// // 
+
+// var color = ['red', 'orange', 'blue', 'violet'];
+
+// var removed = color.splice(2);
+
+// console.log(color);
+// console.log(removed);
+
+// var sum = 0; 
+// var arr = [1, 2, 3];
+// arr.forEach(getSum);
+// console.log(sum);
+
+// function getSum(ele) {
+//    sum += ele;
+// }
+
+// var fruits = ['Apple', 'Orange', 'Kiwi', 'Strawberry'];
+
+// fruits.splice(1, 0, 'Banana');
+
+// console.log(fruits);
+
+// var fruits = ['Apple', 'Orange', 'Kiwi', 'Strawberry'];
+
+// fruits.splice(4, 1, 'Banana');
+
+// console.log(fruits);
+hoisted();
+
+function hoisted() {
+    console.log('Hoisted');
+}
